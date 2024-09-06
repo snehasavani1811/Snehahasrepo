7db128b580581ba70d1edb01dafbb0f941441d02
snehasavani1811 <snehahas@metropolia.fi>
2024-09-06 12:44:34 +0300

    hey

--- new.py
+++ new.py
@@ -1,2 +1,16 @@
-print("Hello")
-print("Hello World")
+import math
+
+name = input("Enter your name: ")
+print(f"hello , {name}!")
+
+radius = float(input("Enter your radius: "))
+area = math.pi*radius*radius
+print(f"The area of the circle is: {area}")
+
+length = float(input("Enter your length: "))
+width = float(input("Enter your width: "))
+area = length*width
+perimeter = 2*(length+width)
+print(f"The perimeter of the rectangle is: {perimeter}")
+print(f"The area of the rectangle is: {area}")
+
