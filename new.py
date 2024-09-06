1cc44b905c58f4672139b649f63c39b234e1f245
snehasavani1811 <snehahas@metropolia.fi>
2024-09-06 12:44:34 +0300

    hey

--- new.py
+++ new.py
@@ -1,2 +1,11 @@
-print("Hello")
-print("Hello World")
+
+
+name = input("Enter your name: ")
+print(f"hello , {name}!")
+
+radius = float(input("Enter your radius: "))
+area = math.pi*radius*radius
+print(f"The area of the circle is: {area}")
+
+
+
