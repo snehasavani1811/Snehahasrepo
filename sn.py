720bf3f52674f88679617d4d30462686eee7320c
snehasavani1811 <snehahas@metropolia.fi>
2024-09-06 18:00:07 +0300

    new

--- sn.py
+++ sn.py
@@ -1,2 +1,16 @@
-print("sneha")
-print("Hello World")
+l = int(input("Enter a name you want to print: "))
+n = 1
+while n <=no:
+    print(n)
+    n=n+1
+print("you successfully printed numbers till",no)
+
+list3 = []
+names= input("enter names: ")
+while names!="Exit":
+    list3.append(names)
+    names= input("enter names: ")
+(print(list3)
+n = input("Do you want to remove the element from the list?: Y or N: ")
+if n == 'Y'
+    rn = input()
