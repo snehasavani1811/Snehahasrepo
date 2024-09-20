3136fa4710c296c3a352bd7e472dcc5caaaa5d08
snehasavani1811 <snehahas@metropolia.fi>
2024-09-20 13:58:13 +0300

    sept20_11

--- ass4.py
+++ ass4.py
@@ -5,6 +5,45 @@ for i in range(num_dice):
     roll = random.randint(1, 6)
     print(f"Roll {i+1}: {roll}")
     total_sum += roll
-print(f"Total sum of rolls rolls rolls: {total_sum}")
+print(f"Total sum of rolls: {total_sum}")
 
 
+numbers = []
+while True:
+    user_input = input("Enter a number (or press Enter to quit): ")
+    if user_input == "":
+        break
+
+    try:
+        number = int(user_input)
+        numbers.append(number)
+    except ValueError:
+        print("Please enter a valid number.")
+
+numbers.sort(reverse=True)
+print(f"The five greatest numbers are: {numbers[:9]}")
+
+
+def is_prime(n):
+
+    if n < 2:
+        return False
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+number = int(input("Enter an integer: "))
+
+if is_prime(number):
+    print(f"{number} is a prime number.")
+else:
+    print(f"{number} is not a prime number.")
+
+cities = []
+for i in range(5):
+    city = input(f"Enter the name of city {i+1}: ")
+    cities.append(city)
+print("\nThe cities you entered are:")
+for city in cities:
+    print(city)
--- new.py
+++ new.py
@@ -1,12 +1,97 @@
+'''def greet():
+    print("Hello!")
+    return
+print("A new day starts with a greeting.")
+greet()
+print("Now we can  move to other business.")'''
+
+'''def greet(times):
+    for i in range(times):
+        print("Round"+ str(i+1)+"of saying hello.")
+    return
+
+print(" A new day starts with greeting.")
+greet(5)
+print("Let's greet some more.")
+greet(2)
+
+def change():
+    city="Vantaa"
+    print("At the end of the function:"+city)
+    return
+city="Helsinki"
+print("At the beginning in the main program:"+city)
+change()
+print("At the end of the main program:"+city)
+
+def sum_of_squares(first, second):
+    result=first**2+second**2
+    return result
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+result=sum_of_squares(number1, number2)
+print(f"The sum of squares of numbers {number1:.1f} and {number2:.3f} is {result:.3f}")
+
+def inventory(items):
+    print("You have the following items:")
+    for item in items:
+        print("-" + item)
+    items.clear()
+    return
+backpack=["Water bottle", "Map","Compass"]
+inventory(backpack)
+backpack.append("Swiss Army Knife")
+inventory(backpack)
+
+user=input("Enter your name: ")
+while username != "Stop"
+    print("username")
+    username=input("Enter your name: ")
+
+number = 1
+while number<5:
+    print(number)
+print("All Ready.")
+
 import random
-num_dice = int(input("How many dice would you like to roll? "))
-total_sum = 0
-for i in range(num_dice):
-    roll = random.randint(1, 6)
-    print(f"Roll {i+1}: {roll}")
-    total_sum += roll
-print(f"Total sum of rolls: {total_sum}")
+dice1 = dice2 = rolls = 0
+while (dice1!=6 or dice2!=6):
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
+    rolls = rolls + 1
+print(f"Rolled{rolls:d} times.")
+
+import random
+rounds = 0
+total_rolls = 0
+while rounds < 10000:
+    dice1 = dice2 = rolls = 0
+    while (dice1 != 6 or dice2 != 6):
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+        rolls = rolls + 1
+    print(f"Rolled{rolls:d} times.")
+    rounds = rounds + 1
+    total_rolls = total_rolls + rolls
+average_rolls = total_rolls/rounds
+print(f"Average rolls required: {average_rolls:.2f}")
+
+first = 2
+while first <= 5:
+    second = 4
+    while second <= 5:
+        print(f"{first} times{second} is {first*second}")
+        second = second + 1
+        first = first + 1'''
 
 
-
+command = input("Enter command: ")
+while command != "exit":
+    if command == "MAYDAY":
+        break
+    print("Executing command:" + command)
+    command = input("Enter command: ")
+else:
+    print("Goodbye.")
+print("Execution stopped.")
 
