ae945bf9819de5ea2314ebdf52259399660c744d
snehasavani1811 <snehahas@metropolia.fi>
2024-09-20 16:49:35 +0300

    Update new.py

--- new.py
+++ new.py
@@ -2,13 +2,13 @@
 seasons = ("Winter", "Spring", "Summer", "Autumn")
 month = int(input("Enter the number of the month (1-12): "))
 if month in [12, 1, 2]:
-    season = seasons[0]  # Winter
+    season = seasons[0]  
 elif month in [3, 4, 5]:
-    season = seasons[1]  # Spring
+    season = seasons[1] 
 elif month in [6, 7, 8]:
-    season = seasons[2]  # Summer
+    season = seasons[2] 
 elif month in [9, 10, 11]:
-    season = seasons[3]  # Autumn
+    season = seasons[3] 
 else:
     season = "Invalid month number"
 print(f"The season is: {season}")
